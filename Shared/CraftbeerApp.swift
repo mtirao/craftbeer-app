06ac48ef2b6ac5267ba79ea3bfa7219dc71ae566
Marcos Tirao <mtirao@icloud.com>
2020-10-23 10:52:44 -0300

    Initial Commit

---
+++ Shared/CraftbeerApp.swift
@@ -0,0 +1,20 @@
+//
+//  CraftbeerApp.swift
+//  Shared
+//
+//  Created by Marcos Tirao on 23/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CraftbeerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
